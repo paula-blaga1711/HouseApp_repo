df8a13910a9fdbd5849a7c01e4514a5a59c7d9d1
Paula Blaga <paula.blaga1711@gmail.com>
2019-05-14 19:08:54 +0300

    created/configured server

--- HouseApp_backEnd/app.js
+++ HouseApp_backEnd/app.js
@@ -1,20 +1,44 @@
 var express = require('express');
+global.app = express();
+app.disable('etag').disable('x-powered-by');
+const bodyParser = require('body-parser');
+
+
 var path = require('path');
+var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/resources', express.static(path.join(__dirname, '/resources')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      status: "error",
+      message: err.message
+    })
+  });
+  
+  
+  app.listen(3500);
+
 
 module.exports = app;
